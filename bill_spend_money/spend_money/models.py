13f387e906b02058f262001702d916f6e73accc3
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-13 15:17:14 -0800

    models update

--- bill_spend_money/spend_money/models.py
+++ bill_spend_money/spend_money/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class product(models.Model):
 	product_name = models.CharField(max_length=200)
+	product_image = models.CharField(max_length=200)
 	product_price = models.IntegerField()
 
 	def __str__(self):
+		return f"{self.product_name} ${product_price} "
+
+class option(models.Model)
