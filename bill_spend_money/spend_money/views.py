9baab3060ece592c7489a519df70751d62671637
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-13 16:59:23 -0800

    views update

--- bill_spend_money/spend_money/views.py
+++ bill_spend_money/spend_money/views.py
@@ -6,7 +6,7 @@ from .models import Products
 # Create your views here.
 def index(request):
     products_list = Products.objects.order_by('-product_price')[:10]
-    template = loader.get_template('spend_money/bill_spend_page.html')
+    template = loader.get_template('bill_spend_page.html')
     context = {
         'products_list': products_list,
     }
