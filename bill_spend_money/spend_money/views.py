c428a823cdd140384714bba72f58fc9e9402e9ea
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-13 16:00:39 -0800

    app spend_mony template,urls,models,views update

--- bill_spend_money/spend_money/views.py
+++ bill_spend_money/spend_money/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,Http404
+from django.template import loader
+from .models import Products
 
 # Create your views here.
+def index(request):
+    products_list = Products.objects.order_by('-product_price')[:10]
+    template = loader.get_template('spend_money/bill_spend_page.html')
+    context = {
+        'products_list': products_list,
+    }
+    return HttpResponse(template.render(context, request))
